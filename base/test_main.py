5ecf33c5189ed79435f79deb9d77e022a1fe3440
Lim Ji Yoon <wldbsvk612@naver.com>
2023-10-11 14:34:29 +0900

    Add files via upload

---
+++ base/test_main.py
@@ -0,0 +1,9 @@
+# import lotto_file
+# arr = lotto_file.fn_lotto(5)
+# from lotto_file import fn_lotto
+from lotto_file import *
+arr = fn_lotto(3)
+print(arr)
+from lotto_file import fn_lotto as l
+import base2.lotto_file
+arr2 = l(10)
